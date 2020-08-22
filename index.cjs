da77c474aabea5c498c87539445aba209faefe1c
MAKSS <kazemi914@live.com>
2020-08-22 21:30:54 +0430

    Package got updated!

--- index.cjs
+++ index.cjs
@@ -1,15 +1,15 @@
-'use strict'
+'use strict';
 function randomString(length) {
-  if (!length) length = Math.ceil(Math.random() * 10 * 2)
+  if (!length) length = Math.ceil(Math.random() * 10 * 2);
   var string = '',
     allowedCharacters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*'
-  const allowedCharactersLength = allowedCharacters.length
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  var allowedCharactersLength = allowedCharacters.length;
   for (var i = 0; i < length; i++)
     string += allowedCharacters.charAt(
       Math.floor(Math.random() * allowedCharactersLength)
-    )
-  return string
+    );
+  return string;
 }
 
-module.exports = randomString
+module.exports = randomString;
